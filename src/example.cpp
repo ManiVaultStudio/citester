be7146a75a2dfb1821e25f5b2d8a80bac9bb720a
Baldur van Lew <b.van_lew@lumc.nl>
2025-03-11 10:29:10 +0100

    Extra functionality in test exe

--- src/example.cpp
+++ src/example.cpp
@@ -3,9 +3,13 @@
  * https://github.com/fmtlib/fmt
 **/
 #include <fmt/chrono.h>
+#include <fmt/ranges.h>
+#include <vector>
 
 int main() {
   auto now = std::chrono::system_clock::now();
   fmt::print("Date and time: {}\n", now);
   fmt::print("Time: {:%H:%M}\n", now);
+  std::vector<int> v = {1, 2, 3};
+  fmt::print("{}\n", v);
 }
